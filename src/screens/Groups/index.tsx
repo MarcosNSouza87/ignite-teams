24b4e57cf52689c84aa02762d8a30e9327538d47
MarcosNSouza87 <m_souza@outlook.com.br>
2024-11-18 18:45:19 -0300

    add_logo

---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import * as RN from 'react-native';
+import styles from './styles';
+
+const Groups = () => {
+  return(
+    <RN.View style={styles.container}>
+      <RN.View>
+        <RN.Text>Ignite 2</RN.Text>
+      </RN.View>
+    </RN.View>
+  )
+}
+
+export default Groups
---
+++ src/screens/Groups/styles.tsx
@@ -0,0 +1,12 @@
+import * as RN from 'react-native';
+
+const styles = RN.StyleSheet.create({
+  container:{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#afafaf',
+  },
+});
+
+export default styles;
